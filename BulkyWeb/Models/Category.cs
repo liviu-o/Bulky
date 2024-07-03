3ac24bf93f7e65128f4f3254e9543b2f37914d9f
liviu-o <li.viu@mail.com>
2024-07-03 12:33:39 +0100

    Server side validations

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -8,8 +8,10 @@ namespace BulkyWeb.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [MaxLength(30)]
         public string Name { get; set; }
         [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "Display Order must be between 1 and 100")]
         public int DisplayOrder { get; set; }
     }
 }
